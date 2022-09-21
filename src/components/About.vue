9028f0599f2f335223c406e11b5ec543ae94f1b0
chandrabumi26 <chandra.bumi@gmail.com>
2022-09-22 05:12:09 +0700

    Last!

--- src/components/About.vue
+++ src/components/About.vue
@@ -74,6 +74,7 @@
             height: 200px;
             width: 200px;
             border-radius: 50% !important;
+            border : 2px solid purple;
         }
 
         &.right{
--- src/components/NFT.vue
+++ src/components/NFT.vue
@@ -2,8 +2,27 @@
     <v-sheet>
         <v-card class="profile">
             <img src="/assets/hushlab-logo.jpg" height="auto">
-            <v-card-title>hUShlab</v-card-title>
+            <v-card-title style="font-weight : bold">hUShlab</v-card-title>
             <v-card-subtitle class="mt-2">10,000 editions of The Mad Scientist Experimental Club from hUShlab, who eager to create a sequence of abnormalities as a New Norm. The Mad Scientist resides in a club on the Ethereum blockchain as a unique non-fungible token (NFT). You will become a part of the sly initiator & creator who always thrive-to-stretch the limit under the roof of hUShlab.</v-card-subtitle>
+        </v-card>
+        <v-card class="mt-5">
+            <v-row>
+                <v-col cols="12" lg="3" class="justify-content-center align-items-center d-flex flex-column">
+                    <v-card-title>What's my Role ?</v-card-title>
+                    <img src="@/assets/profile-picture.jpeg" class="profile-round">
+                    <v-card-subtitle>Blockchain Developer</v-card-subtitle>
+                </v-col>
+                <v-col cols="12" lg="7">
+                    <v-card-subtitle class="mt-15">
+                        As a Blockchain Developer, i'm develop all of 10K Smart Contract with ERC721a (Azuki Based Smart Contract) on Ethereum Block to ensure all of my Holders will get optimized Gas Fee and safest Minting. I also made art generator engine with javascript language. All of metadata's has fulfilled ERC Smart Contract Standard.
+                        I made myself as a Blockchain developer because i want to learn more about web3.0, since this project was created with my own big brother (Art Creative) there is no reason to not get my hand dirty. I didnt deploy all of these art to Opensea.io yet, because of NFT Bear Market, it's hard to make a community. 
+                    </v-card-subtitle>
+
+                    <v-card-subtitle>
+                        I'm still learn more about Blockchain, Especially on Crypto block network. I spent every weekends to learn about this technology ether how it grow or it smacked. Blockchain are not my main focus on Developer fields, but i really love to learn about this like i love my hobby.
+                    </v-card-subtitle>
+                </v-col>
+            </v-row>
         </v-card>
         <v-card class=" mt-5">
             <v-card-title>Mad Scientist Experimental Club Specific Editions ( 1 - 50 )</v-card-title>
@@ -50,14 +69,20 @@ export default {
         padding: 50px;
     }
 
-    .v-card{
-        background: white !important;
-        img{
-            &.nft{
-                border: 2px solid #4baf78;
-                width: 200px;
-            }
+    img{
+
+        &.profile-round{
+            height: 200px;
+            width: 200px;
+            border-radius: 50% !important;
+            border : 2px solid #4baf78;
         }
+    }
+
+    .v-card{
+        border: 2px solid #4baf78 !important;
+        background: white !important;
+        
         &.profile{
             display: flex;
             flex-direction: column;
